276eb624a70e00f9a350b5de254640e480d74de7
Ceren <ingortigno@gmail.com>
2023-02-27 10:56:55 +0300

    Finish the tutorial

--- 4th-grade-event/Assets/Scripts/Draggable.cs
+++ 4th-grade-event/Assets/Scripts/Draggable.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool IsDragging;
+    public Vector3 LastPosition;
+
+    private Collider2D _collider;
+    private DragController _dragController;
+
+    private float _movementTime = 15f;
+    private System.Nullable<Vector3> _movementDestination;
+
+    public string SlotName;
+
     void Start()
     {
-        
+        _collider = GetComponent<Collider2D>();
+        _dragController = GetComponent<DragController>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+    void FixedUpdate()
+    {
+        if (_movementDestination.HasValue)
+        {
+            if (IsDragging)
+            {
+                _movementDestination = null;
+                return;
+            }
+            if (transform.position == _movementDestination)
+            {
+                gameObject.layer = Layer.Default;
+                _movementDestination = null;
+            }
+            else
+                transform.position = Vector3.Lerp(transform.position, _movementDestination.Value, _movementTime * Time.fixedDeltaTime);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Draggable collidedDraggable = collision.GetComponent<Draggable>();
+
+        if (collidedDraggable != null && _dragController.LastDragged.gameObject == gameObject)
+        {
+            ColliderDistance2D colliderDistance2D = collision.Distance(_collider);
+            Vector3 diff = new Vector3(colliderDistance2D.normal.x, colliderDistance2D.normal.y) * colliderDistance2D.distance;
+            transform.position -= diff;
+        }
+        if (collision.CompareTag("DropValid") && collision.name == SlotName)
+        {
+            _movementDestination = collision.transform.position;
+        }
+        else if (collision.CompareTag("DropInvalid"))
+        {
+            _movementDestination = LastPosition;
+        }
+        else
+            _movementDestination = LastPosition;
     }
 }
---
+++ 4th-grade-event/Assets/Scripts/Slot.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public int SlotID;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
